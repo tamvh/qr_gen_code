4301bbb09cf98b8b4c2ac988ad1d939e6fa07105
tam huy <huytamuit@gmail.com>
2018-03-07 16:13:31 +0700

    cap nhat ver

--- app.js
+++ app.js
@@ -14,20 +14,20 @@ theApp.constant('API_URL', API_URL); //define CONST API_URL
         $routeProvider
             .when('/', {
                 controller: 'PayController',
-                templateUrl: 'pay/pay.view.html',
+                templateUrl: 'pay/pay.view.html?v=1.02',
                 controllerAs: 'vm'
             })
             .when('/alert_success/', {
                 controller: 'AlertSuccessController',
-                templateUrl: 'alert.success/alert.success.view.html'
+                templateUrl: 'alert.success/alert.success.view.html?v=1.02'
             })
             .when('/alert_faile/', {
                 controller: 'AlertFaileController',
-                templateUrl: 'alert.faile/alert.faile.view.html'
+                templateUrl: 'alert.faile/alert.faile.view.html?v=1.02'
             })
             .when('/invoice_err/', {
                 controller: 'InvoiceErrController',
-                templateUrl: 'invoice.err/invoice.err.view.html'
+                templateUrl: 'invoice.err/invoice.err.view.html?v=1.02'
             })
             .otherwise({ redirectTo: '/' });
         
